3127d29edb3743bf1e2742a5a464fd1fd3a8250e
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-10-26 11:51:11 -0400

    Adding the basic functionalities of a calculator to the project.

---
+++ logic.js
@@ -0,0 +1,18 @@
+let number1 = 5;
+let number2 = 10;
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
